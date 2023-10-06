95c12767e18e3b28fce01d6f60217a14bdb24d42
Theideabased <ogunmusireseyi01@gmail.com>
2023-10-05 19:43:50 -0700

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,16 +4,16 @@ sudo apt-get update
 sudo apt-get install nginx
 
 # creaye a foldee /data/ if not exists
-mkdir -p /data
+mkdir -p /data/
 
 # create a folder /data/web_static if not exist
-mkdir -p /data/web_static
+mkdir -p /data/web_static/
 
 # create a folder /data/web_static/releases/ if not exist
-mkdir -p /data/web_static/releases
+mkdir -p /data/web_static/releases/
 
 # create a folder /data/web_static/shared/ if not exists
-mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/shared/
 
 # create a folder /data/web_static/releases/test/ if not exist
 mkdir -p /data/web_static/releases/test
@@ -23,10 +23,11 @@ echo "<html>\n <head>\n </head>\n <body>\n Holberton School\n \
 	</body>\n </html>">> /data/web_static/releases/test/index.html
 
 # creating a symbolic link 
-ln -sF /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # give ownership of the /data/ to ubuntu
-sudo chown -R ubuntu :ubuntu /data
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
 # update the nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 printf %s "server {
