c907c4921180464c5a4cd76570bdbc1cdd037b9f
Theideabased <ogunmusireseyi01@gmail.com>
2023-10-20 07:13:45 -0700

    done

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """a program that can print hello world on flask"""
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask("__name__")
 
@@ -43,7 +44,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ a template with the number """
-    return render_template('5-number.html', n=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
