355b1cd7841ba268daf42fc4910bb08af16722aa
saaid323.com <saaid2538@.com>
2023-10-30 08:52:34 +0000

    added requirements

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""script that starts Flask web application"""
 from flask import Flask, jsonify, json
 from models import storage
 from api.v1.views import app_views
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,6 @@
+#!/usr/bin/python3
+"""imports needed """
 from flask import Blueprint
-# import the storage engine needed
-from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.user import User
-from models.place import Place
-from models.review import Review
 
 # import the views used
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
