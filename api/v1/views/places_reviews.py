0129acf8df64c6f8b59f967027e425b7fe83b94f
saaid323.com <saaid2538@.com>
2023-10-30 16:10:12 +0000

    added comments

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""view for Review object that handles all default RESTFul API actions"""
 from flask import abort, jsonify, make_response, request
 from api.v1.views import app_views
 from models.review import Review
@@ -10,6 +11,7 @@ from models import storage
 @app_views.route('/places/<place_id>/reviews', methods=['GET'],
                  strict_slashes=False)
 def get_reviews(place_id):
+    """Retrieves the list of all Review objects of a Place"""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
@@ -21,6 +23,7 @@ def get_reviews(place_id):
 
 @app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
 def get_review(review_id):
+    """Retrieves a Review object"""
     review = storage.get(Review, review_id)
     if not review:
         abort(404)
@@ -30,6 +33,7 @@ def get_review(review_id):
 @app_views.route('/reviews/<review_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_review(review_id):
+    """Deletes a Review object"""
     review = storage.get(Review, review_id)
     if not review:
         abort(404)
@@ -41,6 +45,7 @@ def delete_review(review_id):
 @app_views.route('/places/<place_id>/reviews', methods=['POST'],
                  strict_slashes=False)
 def post_review(place_id):
+    """Creates a Review"""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
@@ -62,6 +67,7 @@ def post_review(place_id):
 
 @app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
 def put_review(review_id):
+    """Updates a Review object"""
     review = storage.get(Review, review_id)
     if not review:
         abort(404)
