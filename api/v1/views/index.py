83e0688874b37d1976611f6ed10adf7d6fe6b2be
saaid323.com <saaid2538@.com>
2023-10-30 09:28:40 +0000

    fixed class error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,8 +10,8 @@ from models.state import State
 from models.user import User
 from models import storage
 
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {"amenity": Amenity, "city": City,
+           "place": Place, "review": Review, "state": State, "user": User}
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
